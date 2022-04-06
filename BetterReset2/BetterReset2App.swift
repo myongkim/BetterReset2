721d3fc2f30124cd2187ffcde3b5e29941efc263
Isaac Kim <myong328@gmail.com>
2022-04-06 09:04:35 -0700

    Initial Commit

---
+++ BetterReset2/BetterReset2App.swift
@@ -0,0 +1,17 @@
+//
+//  BetterReset2App.swift
+//  BetterReset2
+//
+//  Created by Isaac Kim on 4/6/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterReset2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
