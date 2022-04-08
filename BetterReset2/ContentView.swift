056c209f6d2ebeb9846feae71122eb2df10a386e
Isaac Kim <myong328@gmail.com>
2022-04-08 08:58:25 -0700

    spacer issue has been fixed()

--- BetterReset2/ContentView.swift
+++ BetterReset2/ContentView.swift
@@ -12,61 +12,14 @@ struct ContentView: View {
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
     
-    @State private var alertTitle = ""
-    @State private var alertMessage = ""
-    @State private var showingAlert = false
-    
     static var defaultWakeTime: Date {
         var components = DateComponents()
         components.hour = 7
         components.minute = 0
         return Calendar.current.date(from: components) ?? Date.now
     }
-    
-    
-    var body: some View {
-        NavigationView {
-            Form{
-                VStack(alignment: .leading, spacing: 0){
-                    Text("When do you want to wake up?")
-                        .font(.headline)
-                    DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
-                        .labelsHidden()
-                }
-                VStack(alignment: .leading, spacing: 0){
-                    Text("Desired amount of sleep")
-                        .font(.headline)
-                    
-                    Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
-                    
-                    
-                }
-                VStack(alignment: .leading, spacing: 0){
-                    Text("Daily coffee intake")
-                        .font(.headline)
-                    
-                    Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
-                }
-                
-                
-                
-                
-                
-            }
-            .navigationTitle("BetterRest")
-            .toolbar(content: {
-                Button("Calculate", action: calculateBedTime)
-            })
-            .alert(alertTitle, isPresented: $showingAlert) {
-                Button("OK") { }
-            } message: {
-                Text(alertMessage)
-            }
-            
-        }
-    }
-    
-    func calculateBedTime() {
+    var sleepResults: String {
+        // lot of work need to do
         do {
             let config = MLModelConfiguration()
             let model = try SleepCalculator(configuration: config)
@@ -79,19 +32,52 @@ struct ContentView: View {
             let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
             
             let sleepTime = wakeUp - prediction.actualSleep
-            alertTitle = "Your ideal bedtime is..."
-            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
-            
-            
+            return "Your ideal bedtime is " + sleepTime.formatted(date: .omitted, time: .shortened)
             
         } catch {
-            alertTitle = "Error"
-            alertMessage = "Sorry, there was a problem calculating your bedtime."
+            return "There was an error"
             
         }
-        
-        showingAlert = true
     }
+    
+    var body: some View {
+        NavigationView {
+            Spacer()
+            Form{
+                
+                Section("When do you want to wake up?") {
+                    DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                        .labelsHidden()
+                }
+                Section("Desired amount of sleep") {
+                    
+                    Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                }
+                
+                Section("Daily coffee intake") {
+
+                    
+                    Picker("Number of cups", selection: $coffeeAmount) {
+                        ForEach(1..<21) { num in
+                            Text("\(num)")
+                        }
+                    }
+                }
+                Section("the result"){
+                    Text(sleepResults)
+                        .font(.title3)
+                    
+                }
+                
+               
+                
+                
+            }
+            .navigationTitle("BetterRest")
+            Spacer()
+        }
+    }
+    
 }
 
 
